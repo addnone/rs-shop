cb91d26964d264c4af76993a3a1b4f3016867954
addnone <zamkot@yandex.ru>
2021-09-21 10:51:49 +0300

    feat: add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { ShopModule } from './shop/shop.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    ShopModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
