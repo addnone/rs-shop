0ca9c230a31e86ce05e2521399ef61db5bfaac1c
addnone <zamkot@yandex.ru>
2021-10-01 14:16:00 +0300

    feat: add real data in main categoris block

--- src/app/core/components/header/components/main-categories-block/main-categories-block.component.ts
+++ src/app/core/components/header/components/main-categories-block/main-categories-block.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICategory } from 'src/app/shop/models/category.model';
+import { ApiService } from 'src/app/shop/services/api.service';
 
 @Component({
   selector: 'app-main-categories-block',
   templateUrl: './main-categories-block.component.html',
   styleUrls: ['./main-categories-block.component.scss'],
 })
-export class MainCategoriesBlockComponent  {
+export class MainCategoriesBlockComponent implements OnInit  {
+
+  categories: ICategory[] = [];
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getCategories().subscribe((value) => this.categories = value);
+  }
 }
