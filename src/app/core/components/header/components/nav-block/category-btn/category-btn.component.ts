ea5caac6a05ba6b76098ab5ab84c06e9028c8911
addnone <zamkot@yandex.ru>
2021-10-02 14:50:01 +0300

    fix: correct category btn behavior

--- src/app/core/components/header/components/nav-block/category-btn/category-btn.component.ts
+++ src/app/core/components/header/components/nav-block/category-btn/category-btn.component.ts
@@ -1,59 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-btn',
   template: `
-    <a mat-raised-button [routerLink]="[routeLink]">Каталог товаров</a>
+    <a mat-raised-button (click)="onClick()">Каталог товаров</a>
   `,
   styles: [
   ],
 })
-export class CategoryBtnComponent implements OnInit {
-
-  routeLink = '';
+export class CategoryBtnComponent  {
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit(): void {
-    this.routeTo().subscribe((value) => {
-      this.routeLink = value;
-    });
-  }
-
-  isActive() {
-    return this.getCurUrlPath().pipe(
-      map((value) => {
-        return value === 'categories';
-      }),
-    );
-  }
-
-  routeTo() {
-    return this.isActive().pipe(
-      map((value) => {
-        return value ? '' : '/categories';
-      }),
-    );
-  }
-
-  getCurUrlPath() {
-    return this.router.events.pipe(
-      map((value) => {
-        return value;
-      }),
-      filter(e => e instanceof NavigationEnd),
-      map(() => this.route),
-      switchMap(route => {
-        while (route.firstChild) {
-          route = route.firstChild;
-        }
-        return route.url;
-      }),
-      map((value) => {
-        return value[0].path;
-      }),
-    );
+  onClick() {
+    const curRoute = this.route.snapshot.firstChild?.url[0].path;
+    const nextRoute = curRoute === 'categories' ? 'main' : 'categories';
+    this.router.navigate([nextRoute]);
   }
 }
